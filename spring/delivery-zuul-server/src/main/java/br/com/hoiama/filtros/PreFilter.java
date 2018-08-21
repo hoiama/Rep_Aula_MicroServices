609e8daa0b385d513afabfe2120e87216ce86f59
Hoiama Rosa Rodrigues Costa <hoiama9@gmail.com>
2018-08-21 17:29:32 -0300

    Implementando zuul.

---
+++ spring/delivery-zuul-server/src/main/java/br/com/hoiama/filtros/PreFilter.java
@@ -0,0 +1,29 @@
+package br.com.hoiama.filtros;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class PreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType(){
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder(){
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter(){
+        return true;
+    }
+
+    @Override
+    public Object run(){
+
+    }
+
+}
+
+
