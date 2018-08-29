a635a74ea22439c91f63e894742695711ea66368
Hoiama Rosa Rodrigues Costa <hoiama9@gmail.com>
2018-08-29 18:00:19 -0300

    Implementando rabbitMQ (Ta foda, quase lá!!!).

--- spring/delivery-rabbitmq-server/src/main/java/br/com/hoiama/main/Consumer.java
+++ spring/delivery-rabbitmq-server/src/main/java/br/com/hoiama/main/Consumer.java
@@ -1,9 +1,11 @@
 package br.com.hoiama.main;
 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RabbitListener(queues="${jsa.rabbitmq.queue}") //Ouve a fila registrada em "queue" e a envia para "RabbitHandler" ;
 public class Consumer {
 
     /**
@@ -11,7 +13,7 @@ public class Consumer {
      * @Payload: É a anotação que informa que o parâmetro vai receber o corpo da mensagem.
      * @param
      */
-    @RabbitListener(queues="${jsa.rabbitmq.queue}") //É a anotação que marca o método como um listener;
+    @RabbitHandler
     public void receivedMenssage(String msg){
         System.out.println("Recieved Message: " + msg);
     }
--- spring/delivery-rabbitmq-server/src/main/java/br/com/hoiama/main/Producer.java
+++ spring/delivery-rabbitmq-server/src/main/java/br/com/hoiama/main/Producer.java
@@ -1,5 +1,6 @@
 package br.com.hoiama.main;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +13,9 @@ public class Producer {
      * Classe helper para acessar e enviar mensagens para RabbitMQ de forma síncrona;
      */
     @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Value("${jsa.rabbitmq.exchange}")
@@ -20,13 +24,12 @@ public class Producer {
     @Value("${jsa.rabbitmq.routingkey}")
     private String routingKey;
 
-
     /**
-     * Classe helper para acessar e enviar mensagens para RabbitMQ de forma síncrona;
+     * AmqpTemplate: Classe helper para acessar e enviar mensagens para RabbitMQ de forma síncrona;
      * @method rabbitTemplate possui recebe os parâmetro: routingKey(nome da file) e message(mensagem a ser enviada).
      * @param
      */
-    public void produceMenssage(String msg){
+    public void produce(String msg){
         rabbitTemplate.convertAndSend(exchange, routingKey, msg);
         System.out.println("Send msg = " + msg);
     }
