60294ae19749295255045bf2cb09e0626db81792
Hoiama Rosa Rodrigues Costa <hoiama9@gmail.com>
2018-11-09 19:50:29 -0200

    Atualizando Swagger.

--- spring/delivery_angular1_server/src/main/resources/static/app/meus-servicos.js
+++
@@ -1,9 +0,0 @@
-var appService = angular.module('meusServicoModulo', ['ngResource']);
-
-appService.factory('recursoCarona',function ($resource) {
-    return $resource("carona/:idCarona", null,{
-        update : {
-            method: 'PUT'
-        }
-    });
-})
